7f21d32f6418cf1819fe84c6f3080b6c53e0e945
bizualem <ansifabebe971@gmail.com>
2025-03-21 11:39:37 +0000

    adding another page for favorite movie to add

---
+++ frontend/src/pages/favorites.jsx
@@ -0,0 +1,19 @@
+
+
+
+function favorite(){
+
+return (
+<div className="favorites=empty">
+
+<h2>no favorite movies yet</h2>
+<p>start adding movies to your favorites and they will appear here </p>
+
+
+</div>
+
+
+)
+
+}
+export default favorite
