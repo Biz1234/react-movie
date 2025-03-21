825e8472e841565fdf7a3a3672720ace4622eb93
bizualem <ansifabebe971@gmail.com>
2025-03-21 17:10:23 +0000

    fetch data from api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,3 +2,8 @@
 
 const API_KEY = "afa55fa909ddb4c1268779fc487151dd";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+};
