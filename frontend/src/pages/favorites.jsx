516e0eb0b0db2984adc43d345988858e05d4998a
bizualem <ansifabebe971@gmail.com>
2025-03-21 09:21:49 +0000

    adding movie to favorite

---
+++ frontend/src/pages/favorites.jsx
@@ -0,0 +1,25 @@
+import MovieCard from "../components/MovieCard"
+
+
+function Home(){
+
+const movies = [
+    {id: 1, title: "john wick", release_date: "2020"},
+    {id: 2, title: "terminator", release_date: "2013"},
+    {id: 3, title: "the matrix", release_date: "1999"},
+  
+]
+
+
+    return (
+<div className="home">
+<div className="movies-grid">
+    {movies.map(movie =>(<MovieCard movie={movie} key={movie.id} />))}
+</div>
+
+</div>
+
+
+
+    )
+}
