8609eda02bada00694dd2a3accaaffad3a851d17
bizualem <ansifabebe971@gmail.com>
2025-03-21 12:45:39 +0000

    styleshhet my navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,42 +1,46 @@
-
 import MovieCard from "../components/MovieCard";
 import { useState } from "react";
 
-function Home(){
-const [searchQuery, setSearchQuery] = useState("");
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
 
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "2013" },
+    { id: 3, title: "The Matrix", release_date: "1999" },
+  ];
 
-const movies = [
-    {id: 1, title: "john wick", release_date: "2020"},
-    {id: 2, title: "terminator", release_date: "2013"},
-    {id: 3, title: "the matrix", release_date: "1999"},
-  
-];
-const handlesearch = (e) => {
-    e.preventDefault()
-alert(searchQuery)
-setSearchQuery("......")
-};
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+  };
 
-    return (
-<div className="home">
-    <form onSubmit={handlesearch} className="search-form">
-        <input type="text" placeholder="serach for movies.." className="search-input" value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
-<button type="submit" className="search-button">search</button>
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
 
-
-    </form>
-<div className="movies-grid">
-    {movies.map((movie) => movie.title.toLowerCase().startsWith(searchQuery) &&
-        (<MovieCard movie={movie} key={movie.id} />))}
-</div>
-
-</div>
-
-
-
-    )
+      <div className="movies-grid">
+        {movies
+          .filter((movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+          )
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </div>
+  );
 }
-export default Home
+
+export default Home;
